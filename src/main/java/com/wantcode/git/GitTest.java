fbeb1c292097f063007235293c640ff20b224a73
wantCode <wantCode@qq.com>
2022-05-03 17:42:52 +0800

    second commit

--- src/main/java/com/wantcode/git/GitTest.java
+++ src/main/java/com/wantcode/git/GitTest.java
@@ -3,5 +3,6 @@ package com.wantcode.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("哈哈哈哈");
     }
 }
